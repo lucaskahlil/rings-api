8bf3c749095d559d9112aff07082f0e24007aeec
lucaskahlil <kahlilpereira@gmail.com>
2024-09-13 20:24:02 -0300

    move specs

---
+++ src/ring/test/ring.controller.spec.ts
@@ -0,0 +1,20 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { RingController } from './ring.controller';
+import { RingService } from './ring.service';
+
+describe('RingController', () => {
+  let controller: RingController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [RingController],
+      providers: [RingService],
+    }).compile();
+
+    controller = module.get<RingController>(RingController);
+  });
+
+  it('should be defined', () => {
+    expect(controller).toBeDefined();
+  });
+});
